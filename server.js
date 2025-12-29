d9ec100dcd7d928433d29010241eff2edda104a3
Arfa Naaz <arfanaaz6060@gmail.com>
2025-12-29 13:54:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -773,7 +773,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-        "https://havbitadminfrontend.vercel.app",
+        "https://havbitadminfrontend.vercel.app/login",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
