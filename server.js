1f1d7a73e73926febe6fcb17496585254292d6ed
Arfa Naaz <arfanaaz6060@gmail.com>
2025-12-29 22:33:38 +0530

    Fix CORS configuration for allowed origins

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
       "http://localhost:5175",
       "https://havbitadminfrontend.vercel.app",
       "https://habit-ecommerce-three.vercel.app",
-   https://havbit.in
+   "https://havbit.in",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
