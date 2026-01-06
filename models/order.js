ceaab950016e5efe4e12401f8b7b9380426197d4
Arfa Naaz <arfanaaz6060@gmail.com>
2026-01-06 18:48:38 +0530

    Refactor order schema and remove comments

--- models/order.js
+++ models/order.js
@@ -1,52 +1,6 @@
 
 
 
-// const mongoose = require("mongoose");
-
-// const orderSchema = new mongoose.Schema(
-//   {
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-
-//     products: [
-//       {
-//         product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-//         quantity: Number,
-//       },
-//     ],
-
-//     totalAmount: {
-//       type: Number,
-//       required: true,
-//     },
-
-//     status: {
-//       type: String,
-//       enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
-//       default: "Pending",
-//     },
-
-//     orderedAt: { type: Date, default: Date.now },
-
-//     cancelledAt: { type: Date },
-//     cancelledBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Order", orderSchema);
-
-
-
-
-
-
-
-
-
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema(
