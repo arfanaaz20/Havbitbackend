32d6ebebdb710f28682826bdcbd189b1d38a7f8a
Arfa Naaz <arfanaaz6060@gmail.com>
2026-01-06 18:47:28 +0530

    Update VendorSubCategory.js

--- models/VendorSubCategory.js
+++ models/VendorSubCategory.js
@@ -1,31 +1,16 @@
+
+
+
 const mongoose = require("mongoose");
 
 const VendorSubCategorySchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    parent: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-      required: true,
-    },
-
-    vendor: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Vendor",
-      required: true,
-    },
-
-    image: {
-      type: String,
-      default: "",
-    },
+    name: { type: String, required: true, trim: true },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: "VendorCategory", required: true },
+    vendor: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
+    image: { type: String, default: "" },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("VendorSubCategory", VendorSubCategorySchema);
+module.exports = mongoose.models.VendorSubCategory || mongoose.model("VendorSubCategory", VendorSubCategorySchema);
