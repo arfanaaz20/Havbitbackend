6c4c59a1a51aaa233aa8269d4bbed0880da58e1d
Arfa Naaz <arfanaaz6060@gmail.com>
2026-01-06 18:51:00 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,48 +1,30 @@
 
 
-const express = require("express");
-const router = express.Router();
-
-const csvUpload = require("../middleware/csvUpload");
+const router = require("express").Router();
 const cloudUpload = require("../middleware/cloudUpload");
-
 const {
   getProducts,
   getProductById,
   addProduct,
   updateProduct,
-  deleteProduct,
-  exportProductsCSV,
-  importCSV
+  deleteProduct
 } = require("../controllers/productController");
 
-// Export CSV
-router.get("/export/csv", exportProductsCSV);
-
-// Import CSV
-router.post("/import", csvUpload.single("file"), importCSV);
-
-// Get all
 router.get("/", getProducts);
-
-// Get by ID
 router.get("/:id", getProductById);
 
-// Add product
 router.post(
   "/",
   cloudUpload.fields([{ name: "image" }, { name: "logo" }]),
   addProduct
 );
 
-// Update product
 router.put(
   "/:id",
   cloudUpload.fields([{ name: "image" }, { name: "logo" }]),
   updateProduct
 );
 
-// Delete
 router.delete("/:id", deleteProduct);
 
 module.exports = router;
